b5b38798a3a8a5593db0404a518873aac05eeb39
Elijah Munroe <elimunroe15@hotmail.com>
2021-01-28 11:00:16 -0500

    Asking for name

--- src/controlFlow/Main.java
+++ src/controlFlow/Main.java
@@ -1,11 +1,20 @@
 package controlFlow;
 
+import java.util.Scanner;
+
 public class Main {
 
 	public static void main(String[] args) {
 		AsciiChars.printNumbers();
 		AsciiChars.printUpperCase();
 		AsciiChars.printLowerCase();
+		
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Please enter your name: ");
+		String name = scanner.next();
+		System.out.println("Hello " + name);
+		
+		scanner.close();
 	}
 
 }
